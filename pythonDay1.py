a3c35018de0f93c80715fae8f9d7e4d32f477ec0
Anandugalmugale <anandugalmugale03@gmail.com>
2024-09-30 12:34:33 +0530

    Add files via upload

---
+++ pythonDay1.py
@@ -0,0 +1,47 @@
+from functools import reduce
+
+name="anand"
+age=21
+print(name)
+print(age)
+
+#this is comment single line
+
+""" this is multipale comment """
+
+a="red"
+b="blue"
+print(a)
+print(b)
+
+f=20
+t=2.45
+name="hello"
+login=True
+print(type(f), type(t),type(name),type(login))
+
+firstname='anand'
+lastname='ugalmugale'
+print(firstname+lastname)
+
+score=10
+print(score)
+score=20
+print(score)
+
+a=3
+b=4
+print(a+b)
+
+
+
+
+
+userinput=input('enter your age :- ')
+# print(f'your age is {userinput}')
+print('your age',userinput)
+
+
+is_student=True
+print(is_student)
+
